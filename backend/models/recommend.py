7391efe61c493195512ba87cf8420876f92cd56d
Ishaan Singla <singlaishaan.09@gmail.com>
2026-07-09 14:55:20 +0530

    Added performance score and reccomendation function

--- backend/models/recommend.py
+++ backend/models/recommend.py
@@ -42,9 +42,69 @@ df.to_csv(
     index=False
 )
 
-
 DATA_PATH = "data/recommendation_data.csv"
 WINDOW_PERCENT = 0.10
 
+PROCESSOR_SCORE = {
+    "apple": 10,
+    "snapdragon": 9,
+    "google": 8,
+    "mediaTek": 7,
+    "exynos": 6,
+    "unisoc": 5
+}
+
+
 phones_df = pd.read_csv(DATA_PATH)
 
+def filter_by_budget(predicted_price):
+    
+    lower_limit = predicted_price * (1 - WINDOW_PERCENT)
+    upper_limit = predicted_price * (1 + WINDOW_PERCENT)
+
+    filtered_df = phones_df[
+        (phones_df["price"] >= lower_limit)
+        &
+        (phones_df["price"] <= upper_limit)
+    ]
+
+    return filtered_df
+
+def score_performance(filtered_df):
+
+    filtered_df = filtered_df.copy()
+
+    filtered_df["processor_score"] = (
+        filtered_df["processor_brand"]
+        .map(PROCESSOR_SCORE)
+    )
+
+    filtered_df["performance_score"] = (
+    filtered_df["ram"] * 0.40 +
+    filtered_df["storage"] * 0.25 +
+    filtered_df["processor_score"] * 0.20 +
+    filtered_df["num_cores"] * 0.15
+    )
+
+    filtered_df = filtered_df.sort_values(
+    by="performance_score",
+    ascending=False
+    )
+
+    return filtered_df.head(5)
+
+if __name__ == "__main__":
+
+    filtered = filter_by_budget(38000)
+
+    recommendations = score_performance(filtered)
+
+    print(
+        recommendations[
+            [
+                "phone_name",
+                "price",
+                "performance_score"
+            ]
+        ]
+    )

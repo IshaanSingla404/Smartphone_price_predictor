f0e1ca394633522a8e2e81d8e5ef7804c622c76a
Ishaan Singla <singlaishaan.09@gmail.com>
2026-07-10 14:31:28 +0530

    api

--- frontend/js/loading.js
+++ frontend/js/loading.js
@@ -66,7 +66,7 @@ if (!storedSpecs) {
 } else {
   const phoneSpecs = JSON.parse(storedSpecs);
 
-  fetch('https://forge-backend-dh8s.onrender.com', {
+  fetch('https://forge-backend-dh8s.onrender.com/predict', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(phoneSpecs)
